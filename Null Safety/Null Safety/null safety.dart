a36244cb2166d0a3377817aadafd1b43f1f5da07
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-02-10 12:33:03 +0600

    null safety

--- Null Safety/Null Safety/null safety.dart
+++ Null Safety/Null Safety/null safety.dart
@@ -6,4 +6,38 @@ void main(){
   if(name == null){
     print('Name is null');
   }
+
+  String name1 = name ?? 'Stranger';
+  print(name1);
+
+  List<int?> items = [1,2,null,4];
+  print(items);
+
+  printAddress(null);
+
+  Person person = Person(null);
+
+  Profile profile = Profile(null, null);
+  profile.printProfile();
+}
+
+void printAddress(String? address){
+  print(address);
+}
+
+class Person {
+  String? name;
+  Person(this.name);
+}
+
+class Profile{
+  String? name;
+  String? bio;
+
+  Profile(this.name, this.bio);
+
+  void printProfile(){
+    print('Name ${name ?? 'Unknown'}');
+    print('Bio ${bio ?? 'None Provided'}');
+  }
 }
