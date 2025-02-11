8d6db4af1958b70ca03053000353b28c663465b6
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-02-11 11:29:34 +0600

    late keyword

--- Null Safety/Late Keyword.dart
+++ Null Safety/Late Keyword.dart
@@ -3,4 +3,26 @@ late String name;
 void main(){
   name = 'Jhon';
   print(name);
+
+  Person person = Person();
+  person.name = 'Nafi';
+  person.greet();
+
+  print('Starting');
+  late String value = provideCountry();
+  print('End');
+  print(value);
+}
+
+class Person{
+  late String name;
+
+  void greet(){
+    print('Hello $name');
+  }
+}
+
+String provideCountry(){
+  print('Function is called');
+  return 'Usa';
 }
